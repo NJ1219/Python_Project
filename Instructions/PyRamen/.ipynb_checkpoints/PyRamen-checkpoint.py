4664dc0b4a87f8c30eded7bae4961ff8972a1221
NJ1219 <niteshjain1219@gmail.com>
2020-06-11 12:57:53 -0400

    PyBank and PyRamen Assignment Submissions

--- Instructions/PyRamen/.ipynb_checkpoints/PyRamen-checkpoint.py
+++ Instructions/PyRamen/.ipynb_checkpoints/PyRamen-checkpoint.py
@@ -32,21 +32,18 @@ with open(menu_filepath, 'r') as menufile:
         description= line[2]
         price= float(line[3])
         cost= float(line[4])
-    # @TODO: Calculate profit of each item in the menu data
+ 
+# @TODO: Calculate profit of each item in the menu data
         profit = price - cost
         
         line.append(profit)
         menu.append(line)
         
-       
-        
- #to print the menu list of lists   
+ 
+ #To print the menu list of lists   
     print(menu)
     print("\n")
     
-
-
-
 
 # @TODO: Initialize dict object to hold our key-value pairs of items and metrics
 report = {}
@@ -54,28 +51,26 @@ report = {}
 # Initialize a row counter variable
 row_count = 0
 
-
-
 # @TODO: Read in the sales data into the sales list
 
 with open(sales_filepath, 'r') as salesfile:
-    csvreader=csv.reader(salesfile, delimiter=',')
+        csvreader=csv.reader(salesfile, delimiter=',')
     
-    header = next(csvreader)
-    print(header)
+        header = next(csvreader)
+        print(header)
  
-    for line in csvreader:
+        for line in csvreader:
          # To view the contents of the csvfile print(line)
         
-     # Line_Item_ID,Date,Credit_Card_Number,Quantity,Menu_Item
-    # @TODO: Initialize sales data variables
+      # Line_Item_ID,Date,Credit_Card_Number,Quantity,Menu_Item
+      # @TODO: Initialize sales data variables
    
-        line_num = int(line[0])
-        date= line[1]
-        cc_num= int(line[2])
-        quantity= float(line[3])
-        menu_item= line[4]
-        sales.append(line)
+            line_num = int(line[0])
+            date= line[1]
+            cc_num= int(line[2])
+            quantity= float(line[3])
+            menu_item= line[4]
+            sales.append(line)
         
  #   print(sales[:10])
 
@@ -84,19 +79,43 @@ with open(sales_filepath, 'r') as salesfile:
     # @TODO:
     # If the item value not in the report, add it as a new entry with initialized metrics
     # Naming convention allows the keys to be ordered in logical fashion, count, revenue, cost, profit
-        if menu_item not in report.keys():
-            report[menu_item] = {"01-count": quantity, "02-revenue" :price * quantity, "03-cogs": quantity * cost, "04-profit": quantity * profit}
-        else:
-            report[menu_item]["01-count"] += quantity
-            report[menu_item]["02-revenue"] += price* quantity
-            report[menu_item]["03-cogs"] += cost * quantity
-            report[menu_item]["04-profit"] += profit * quantity
+            if menu_item not in report.keys():
+                report[menu_item] = {"01-count": quantity, "02-revenue" : 0, "03-cogs": 0, "04-profit": 0
+  
+            #If the item is in the report, increase the count of quantity, price, cost and profit    
+            else:
+                report[menu_item]["01-count"] += quantity
+                
         
+        for a, b in report.items():
+            print (a,b)
+    
+   # Loop through the list containing sales data  
+    for sales_row in sales:
+        sale_item = sales_row[4]
+        quantity = sales_row[3]
+        
+        # Loop through the list conatining menu data
+        for menu_row in menu:
+            menu_item = menu_row[0]
+            price = menu_row[3]
+            cost = menu_row[4]
+            
+            # Comparing the data in both the lists to get the desired results
+            if sale_item == menu_item:
+                report[menu_item]["02-revenue"] += (float(price) * int(quantity))
+                report[menu_item]["03-cogs"] += (float(cost) * int(quantity))
+                report[menu_item]["04-profit"] += (float(price)-float(cost))* int(quantity)
+            elif sale_item != menu_item:
+                continue  
+    
+    # Printing the results on the console            
     for a, b in report.items():
         print (a,b)
+        
             
       
-# output file with metrics
+# @TODO: Write out report to a text file (won't appear on the command line output)
 output_path = Path("../../PyRamen/PyRamen.txt")
     
 with open(output_path, 'w') as file:
@@ -105,65 +124,10 @@ with open(output_path, 'w') as file:
         file.write(f"{key} {report[key]} \n")
     
 
-    # @TODO: For every row in our sales data, loop over the menu records to determine a match
-
-
-        # Item,Category,Description,Price,Cost
-        # @TODO: Initialize menu data variables
-
-cogs = 0
-revenue = 0
-
-    
+   
         
 
-        # @TODO: If the item value in our sales data is equal to the any of the items in the menu, then begin tracking metrics for that item
-   # for key, values in report.items():
-    #    for item in menu:
-            
-     #       if key == item[0]:
-      #          report[menu_item] = {'01-count': quantity, '02-revenue': quantitiy*price, '03-cogs':quantity*cost, '04-profit':quantity*profit}
-                # if menu_item not in report.keys():
-               #     report[menu_item] = {"01-count": quantity}
-               # else:
-               #     report[menu_item]["01-count"] += quantity
-                
-                 
-       #         report[key] = {'01-count': quantity, '02-revenue': quantity*item[], '03-cogs':quantity*cost, '04-profit':quantity*profit}
-                             
-        #    else:
-         #       continue
-            
-            
-               
-   # print (report)
-            
-           
-            
-
-            # @TODO: Print out matching menu data
 
 
 
 
-
-
-            # @TODO: Cumulatively add up the metrics for each item key
-
-
-
-
-
-        # @TODO: Else, the sales item does not equal any of the item in the menu data, therefore no match
-
-
-
-    # @TODO: Increment the row counter by 1
-
-
-# @TODO: Print total number of records in sales data
-
-
-
-
-# @TODO: Write out report to a text file (won't appear on the command line output)
--- Instructions/PyRamen/PyRamen.py
+++ Instructions/PyRamen/PyRamen.py
@@ -39,9 +39,8 @@ with open(menu_filepath, 'r') as menufile:
         line.append(profit)
         menu.append(line)
         
-       
-        
- #to print the menu list of lists   
+ 
+ #To print the menu list of lists   
     print(menu)
     print("\n")
     
@@ -52,28 +51,26 @@ report = {}
 # Initialize a row counter variable
 row_count = 0
 
-
-
 # @TODO: Read in the sales data into the sales list
 
 with open(sales_filepath, 'r') as salesfile:
-    csvreader=csv.reader(salesfile, delimiter=',')
+        csvreader=csv.reader(salesfile, delimiter=',')
     
-    header = next(csvreader)
-    print(header)
+        header = next(csvreader)
+        print(header)
  
-    for line in csvreader:
+        for line in csvreader:
          # To view the contents of the csvfile print(line)
         
-  # Line_Item_ID,Date,Credit_Card_Number,Quantity,Menu_Item
-  # @TODO: Initialize sales data variables
+      # Line_Item_ID,Date,Credit_Card_Number,Quantity,Menu_Item
+      # @TODO: Initialize sales data variables
    
-        line_num = int(line[0])
-        date= line[1]
-        cc_num= int(line[2])
-        quantity= float(line[3])
-        menu_item= line[4]
-        sales.append(line)
+            line_num = int(line[0])
+            date= line[1]
+            cc_num= int(line[2])
+            quantity= float(line[3])
+            menu_item= line[4]
+            sales.append(line)
         
  #   print(sales[:10])
 
@@ -82,17 +79,40 @@ with open(sales_filepath, 'r') as salesfile:
     # @TODO:
     # If the item value not in the report, add it as a new entry with initialized metrics
     # Naming convention allows the keys to be ordered in logical fashion, count, revenue, cost, profit
-        if menu_item not in report.keys():
-            report[menu_item] = {"01-count": quantity, "02-revenue" :price * quantity, "03-cogs": quantity * cost, "04-profit": quantity * profit}
-    #If the item is in the report, increase the count of quantity, orice, cost and profit    
-        else:
-            report[menu_item]["01-count"] += quantity
-            report[menu_item]["02-revenue"] += price* quantity
-            report[menu_item]["03-cogs"] += cost * quantity
-            report[menu_item]["04-profit"] += profit * quantity
+            if menu_item not in report.keys():
+                report[menu_item] = {"01-count": quantity, "02-revenue" : 0, "03-cogs": 0, "04-profit": 0
+  
+            #If the item is in the report, increase the count of quantity, price, cost and profit    
+            else:
+                report[menu_item]["01-count"] += quantity
+                
         
+        for a, b in report.items():
+            print (a,b)
+    
+   # Loop through the list containing sales data  
+    for sales_row in sales:
+        sale_item = sales_row[4]
+        quantity = sales_row[3]
+        
+        # Loop through the list conatining menu data
+        for menu_row in menu:
+            menu_item = menu_row[0]
+            price = menu_row[3]
+            cost = menu_row[4]
+            
+            # Comparing the data in both the lists to get the desired results
+            if sale_item == menu_item:
+                report[menu_item]["02-revenue"] += (float(price) * int(quantity))
+                report[menu_item]["03-cogs"] += (float(cost) * int(quantity))
+                report[menu_item]["04-profit"] += (float(price)-float(cost))* int(quantity)
+            elif sale_item != menu_item:
+                continue  
+    
+    # Printing the results on the console            
     for a, b in report.items():
         print (a,b)
+        
             
       
 # @TODO: Write out report to a text file (won't appear on the command line output)
